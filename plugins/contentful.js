1758577c88f47690b69198bb706c232e271c3832
Alex Valle <alex.valle@gmail.com>
2019-04-28 18:52:50 -0400

    succesfully getting blog posts from contentful

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,7 +1,8 @@
 const contentful = require('contentful')
+
 const config = {
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_DELIVERY_API
+  space: process.env.CTF_SPACE_ID,
+  accessToken: process.env.CTF_CD_ACCESS_TOKEN
 }
 
 const client = contentful.createClient(config)

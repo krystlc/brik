e737f2fdfed789cc00c01d90a96a76b7af92ffd0
Alex Valle <alex.valle@gmail.com>
2019-04-29 11:34:00 -0400

    successful prototype calls

---
+++ pages/prototype/__prototype.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="!isLoading">
+      <pre>{{ currentPrototype.fields }}</pre>
+    </div>
+    <div v-else>
+      <span>loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    currentPrototype() {
+      return this.$store.state.entry.currentEntry
+    },
+    isLoading() {
+      return this.$store.state.entry.isLoading
+    }
+  },
+  async fetch({ store, params }) {
+    await store.dispatch('entry/getEntryBySlug', 'prototype', params.slug)
+  }
+}
+</script>
---
+++ pages/prototype/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <h1>Prototypes</h1>
+      <ul>
+        <li v-for="(prototype, i) in prototypes" :key="i">
+          <nuxt-link :to="`/prototype/${prototype.fields.slug}`">
+            {{ prototype.fields.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  computed: {
+    prototypes() {
+      return this.$store.state.prototypes.prototypes
+    }
+  },
+  async fetch({ store, params }) {
+    await store.dispatch('prototypes/getPrototypes', params.slug)
+  }
+}
+</script>

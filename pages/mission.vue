04545f1bcbaa6ffa2416d8326f8ff6d01924b20a
Alex Valle <alex.valle@gmail.com>
2019-04-29 15:04:08 -0400

    added entryMixin to non-simple pages

--- pages/mission.vue
+++ pages/mission.vue
@@ -1,7 +1,21 @@
 <template>
   <v-layout>
     <v-flex text-xs-center>
-      <h1>Mission</h1>
+      <h1>{{ entry.fields.title }}</h1>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import mixin from '@/mixins/entryMixin'
+
+export default {
+  mixins: [mixin],
+  async fetch({ store, params }) {
+    await store.dispatch('entry/getEntryBySlug', {
+      type: 'page',
+      slug: 'mission'
+    })
+  }
+}
+</script>

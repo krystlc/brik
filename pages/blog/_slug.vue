9f39f1b33f016e2100e366426604843e6dc2a859
Alex Valle <alex.valle@gmail.com>
2019-05-17 08:45:22 -0400

    added content class to v-html parent

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -20,8 +20,8 @@
     <section class="section">
       <div class="container">
         <div class="columns">
-          <div class="column is-half-desktop">
-            <div class="content" v-html="copy"></div>
+          <div class="column is-half-desktop content">
+            <div v-html="copy"></div>
           </div>
         </div>
       </div>

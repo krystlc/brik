5fc9c08f962bd54220cca0ca5c2cd8493023a57f
Alex Valle <alex.valle@gmail.com>
2019-05-18 14:33:20 -0400

    adjusted text display

--- pages/subscribed.vue
+++ pages/subscribed.vue
@@ -1,8 +1,8 @@
 <template>
   <main id="mission">
     <section class="section">
-      <div class="content">
-        <h1 class="title">Subscribed!</h1>
+      <div class="container content has-text-centered-desktop">
+        <h1 class="title has-text-weight-light">Subscribed!</h1>
         <p>
           Awesome, we'll make sure to keep you up to date with any big news or
           events!
